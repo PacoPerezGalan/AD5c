5cdcf76518af42641b31b25b2d37db9488b12f2f
pacoperezgalan <quico.97@gmail.com>
2017-01-31 23:34:53 +0100

    5c acabat

--- AD5c/src/ad5c/Program.java
+++ AD5c/src/ad5c/Program.java
@@ -31,23 +31,24 @@ public class Program {
 		
 		
 		//PER A AGAFAR/CONSULTAR
+		
 		session.beginTransaction();
 		
-		Empresa em=session.get(Empresa.class, "123456781");
-		if(em!=null)
-		System.out.println("Hem recuperat de Empresa: "+em.getNombre());//+" "+em.getCif()+" "+em.getEmpleados());
-		else
-			System.out.println("Es null");
+		Empresa em=session.get(Empresa.class, "12345678");
 		
-		/*
+		System.out.println("Hem recuperat de Empresa: "+em.getNombre());//+" "+em.getCif()+" "+em.getEmpleados());
+		
+		
+		
 		for(Pedido pedido : em.pedidos ){
 			System.out.println("Pedido: "+pedido.getEmpresa().cif+" "+pedido.getFecha());
 		}
 		
 		
 		System.out.println("Direccion: "+em.getDireccion().getCalle()+" "+em.getDireccion().getCP()+" "+em.getDireccion().getPoblacion());
-		*/
+		
 		session.getTransaction().commit();
+		
 		//
 		session.close();
 		HibernateUtilities.getSessionFactory().close();
